8bea4d3523f1d528bfa775fed8ddbb88b2636a3a
kazeem jamiu <kazeemj565@gmail.com>
2025-02-14 11:57:41 +0100

    Adding missing-api-endpoint adjustment

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,12 +48,12 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
-@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def get_book(book_id: int):
-    book = db.get_book(book_id)
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return book
+# @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+# async def get_book(book_id: int):
+#     book = db.get_book(book_id)
+#     if not book:
+#         raise HTTPException(status_code=404, detail="Book not found")
+#     return book
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
@@ -69,6 +69,3 @@ async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
-
-# @router.get("/{book_id}", status_code=status.HTTP_404_NOT_FOUND)
-# async def get_book_by_id(book_id: int) -> None:
